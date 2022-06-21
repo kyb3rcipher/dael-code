a6f2beb9a59c6b570e257e232c0bf57664142d7f
Kyb3r Cipher <me@kyb3rcipher.com>
2022-06-20 22:45:46 -0500

    Fixing love.cpp <3

--- love.cpp
+++ love.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
 int main () {
-cout << “my boyfriend is cool” << endl << “i love you, gael”;
+cout << "my boyfriend is cool" << endl << "i love you, gael";
 return 0;
 }
